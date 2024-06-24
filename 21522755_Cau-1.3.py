56fea35d2f4a28d1c4ee373b8c4b81171864f7a3
Nguyen Manh Tuan <163387762+IzukawaJames2607@users.noreply.github.com>
2024-06-24 19:23:10 +0700

    Update 21522755_Cau-1.3.py

--- 21522755_Cau-1.3.py
+++ 21522755_Cau-1.3.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.title("Random Words Generator")
-st.title("")
+st.header("Random Word")
+st.header("-")
+st.text("Meaning: -")
+st.text("Click the Generate button to generate new word")
+st.button("Generate")
