4bbcae04144eb84e35455d55ee4517ddfe824a17
Nguyen Manh Tuan <163387762+IzukawaJames2607@users.noreply.github.com>
2024-06-24 11:08:45 +0700

    Update 21522755_Cau-2.4.py

--- 21522755_Cau-2.4.py
+++ 21522755_Cau-2.4.py
@@ -1,11 +1,3 @@
-import subprocess
-import sys
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install('seaborn')
-
 import streamlit as st
 import pandas as pd
 import seaborn as sns
